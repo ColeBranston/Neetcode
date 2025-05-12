8f39c1ccd4cde43a0bbccfaf4300fc78cab1d6af
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2025-05-12 04:45:58 -0400

    first couple of easy's

---
+++ ContainsDuplicate.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        stor = set()
+        for n in nums:
+            stor.add(n)
+
+        return not (len(stor) == len(nums))
