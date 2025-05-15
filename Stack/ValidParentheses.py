4ca2c162d4312d08a8c98e30eda8c392d4c92be3
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2025-05-15 17:37:17 -0400

    completed an easy

--- Stack/ValidParentheses.py
+++ Stack/ValidParentheses.py
@@ -1,16 +1,7 @@
-class stack:
-    def __init__(self, array):
-        self.stor = array
-
-    def push(self, val):
-        self.stor.append(val)
-
-    def pop(self):
-        temp = self.stor[-1]
-        del self.stor[-1]
-        return temp
-
-
 s = "([{}])"
 
-for i in range(len(s)//2):
+stor = {
+    "(": [],
+    "{": [],
+    "[": []
+}
